060605a695fffe63061b236c1a55ab3a3f8236d9
Alex Kassil <alexkassil@sbcglobal.net>
2017-09-13 14:34:53 -0700

    Create primitive, do_move, and generate_moves

--- solve.py
+++ solve.py
@@ -1,5 +1,25 @@
 def main():
-    print("Hello World")
+    init_pos = 4
+    print(init_pos)
+#   print(solve(init_pos, primitive, generate_moves, do_move))
+
+def primitive(pos):
+    if pos == 0:
+        return 'L'
+    return 'U'
+
+def do_move(pos, action):
+    return pos - action
+
+def generate_moves(pos):
+    moves = []
+    if pos >= 1:
+        moves += [1]
+    if pos >= 2:
+        moves += [2]
+    if pos >= 3:
+        moves += [3]
+    return moves
 
 if __name__ == "__main__":
     main()
